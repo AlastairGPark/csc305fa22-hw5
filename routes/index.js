b8e7214850bb119428791b555826fc550ec2e397
Alastair Park <aigpark808@gmail.com>
2022-11-15 15:00:01 -0500

    complete

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { postdata: undefined });
+});
+
+router.post('/', function(req, res, next) {
+  console.log(JSON.stringify(req.body));
+  res.render('index', { postdata: req.body });
 });
 
 module.exports = router;
